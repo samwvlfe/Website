3eadf83995d32c1c9d807ae3ecac081d3827571e
285761 <sam.wolfe@spartannash.com>
2024-08-06 11:52:17 -0400

    New Resi changes

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-function showMeStuff(){
-    let image = document.querySelector('.leftside img');
-    image.style.width = '300px';
-    image.style.width = '350px';
-    image.style.marginLeft = '15.5vw';
-    image.style.marginTop = '8vh';
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-    const homeButton = document.getElementById('Me');
-    homeButton.onclick = showMeStuff()
-});
-
 //make left side of screen bigger for menu items
 document.addEventListener('DOMContentLoaded', () => {
     const path = window.location.pathname;
